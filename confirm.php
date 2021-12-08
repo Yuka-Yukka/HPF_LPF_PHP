69605b7a84e23d69159add0c17ac6d1983933cb2
Yuka-Yukka <95258095+Yuka-Yukka@users.noreply.github.com>
2021-12-08 23:42:53 +0900

    Add files via upload

---
+++ confirm.php
@@ -0,0 +1,29 @@
+<?php require_once('filter.php') ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="result">
+    <?php
+     if(isset($_POST['calculate'])){
+        $HPFfrequency =$_POST['HPF_value'];
+        $LPFfrequency =$_POST['LPF_value'];
+    }
+
+        if((@$HPFfrequency && @$LPFfrequency) != 0){
+        @$HPFcalculate = floor(1.5708 / (2*pi()*$HPFfrequency));
+        @$LPFcalculate = floor(1 / (2.256*$LPFfrequency));
+        }
+
+    ?>
+    <p>ハイパスフィルタ計算結果<?php echo @$HPFcalculate?></p>
+    <p>ローパスフィルタ計算結果<?php echo @$LPFcalculate?></p>
+    </div>
+</body>
+</html>
---
+++ filter.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="tytle">
+        <?='デジタルフィルタの設計'?><br />
+    </div>
+    <div class="about">
+        <?='ここではLPF,HPFの値設定または、値から周波数を求めます。'?><br />
+    </div>
+
+    <form action="confirm.php" method = "post">
+        <div class="HPF">
+            <?='HPF'?>
+            <input type="number" value="HPF_value" size="15">
+        </div>
+        <div class="LPF">
+            <?='LPF'?>
+            <input type="number"  value="LPF_value" size="15">
+        </div>
+
+        <div class="button">
+            <input type="submit"  value="計算">
+        </div>
+    </form>
+
+</body>
+</html>
